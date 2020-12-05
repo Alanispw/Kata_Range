96a7b76b1f9661e003851d687006b6902fc871a2
Alanis Pereyra <alaniswagner14@hotmail.com>
2020-12-04 23:32:38 -0800

    Parse input correctly passed

--- src/__tests__/index.js
+++ src/__tests__/index.js
@@ -22,4 +22,12 @@ describe('range', () => {
         const range = new Range();
         expect(range.readInput('/tests/r1')).toEqual('(2,4]');
     });
+    it('Parse input correctly', () => {
+        const range = new Range();
+        expect(range.parseInput(range.readInput('/tests/points'))).toEqual({
+            'leftP' : '{',
+            'nums' : [-1,1,6,10],
+            'rightP' : '}',
+        });
+    });
 });
--- src/index.js
+++ src/index.js
@@ -4,4 +4,11 @@ export default class Range {
     readInput(path){
         return fs.readFileSync(path, 'utf8');
     }
+    parseInput(input) {
+        const obj = {};
+        obj.leftP = input[0];
+        obj.rightP = input[input.length - 1];
+        obj.nums = input.substring(1, input.length - 1).split(',').map(num => Number(num));
+        return obj;
+    }
 }
